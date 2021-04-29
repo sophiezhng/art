b304e1a8086573d4cae074e6151dc771fbca5d1a
Sophie Zhang <sophie.zhangg@icloud.com>
2021-04-29 19:19:03 -0400

    Added slideshow feature

--- js/script.js
+++ js/script.js
@@ -7,11 +7,14 @@ var modalImg = document.getElementById("img01");
 // and the caption in the modal
 var captionText = document.getElementById("caption");
 
+var num = 0;
+
 // Go through all of the images with our custom class
 for (var i = 0; i < images.length; i++) {
   var img = images[i];
   // and attach our click listener for this image.
   img.onclick = function(evt) {
+    num = i;
     modal.style.display = "block";
     modalImg.src = this.src;
     captionText.innerHTML = this.alt;
@@ -19,8 +22,30 @@ for (var i = 0; i < images.length; i++) {
 }
 
 var span = document.getElementsByClassName("close")[0];
+var prev = document.getElementsByClassName("prev")[0];
+var next = document.getElementsByClassName("next")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
+}
+
+prev.onclick = function() {
+  num--;
+  if (num <= 0) {
+    num = images.length - 1;
+  }
+  var img = images[num];
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
+}
+
+next.onclick = function() {
+  num++;
+  if (num >= images.length - 1) {
+    num = 0;
+  }
+  var img = images[num];
+  modalImg.src = img.src;
+  captionText.innerHTML = img.alt;
 }
