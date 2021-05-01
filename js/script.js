922c0ec8cb18b04b0971c42d9a87419f044cde74
Sophie Zhang <sophie.zhangg@icloud.com>
2021-04-30 22:50:34 -0400

    Add light and dark mode

--- js/script.js
+++ js/script.js
@@ -50,3 +50,29 @@ next.onclick = function() {
   modalImg.src = img.src;
   captionText.innerHTML = img.alt;
 }
+
+ // function to set a given theme/color-scheme
+ function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.documentElement.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+  if (localStorage.getItem('theme') === 'dark-theme') {
+      setTheme('light-theme');
+  } else {
+      setTheme('dark-theme');
+  }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+  if (localStorage.getItem('theme') === 'dark-theme') {
+      setTheme('dark-theme');
+      document.getElementById('slider').checked = false;
+  } else {
+      setTheme('light-theme');
+    document.getElementById('slider').checked = true;
+  }
+})();
